ec804e0164b3ae4ee54d67ae5c8366c334fa65ec
Sevan Nerse <nerse.sevan@gmail.com>
2022-01-09 14:46:10 +0300

    pages structure constructed

---
+++ pages/_lang/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <article>
+    <h1>{{ page.title }}</h1>
+    <nuxt-content :document="page" />
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  async asyncData ({ $content, params }) {
+    const page = await $content('/' + params.lang, 'about').fetch()
+
+    return {
+      page
+    }
+  }
+}
+</script>
---
+++ pages/_lang/posts/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <article>
+    <h1>{{ page.title }}</h1>
+    <nuxt-content :document="page" />
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  async asyncData ({ $content, params }) {
+    const page = await $content('/' + params.lang + '/posts', params.slug).fetch()
+
+    return {
+      page
+    }
+  }
+}
+</script>
