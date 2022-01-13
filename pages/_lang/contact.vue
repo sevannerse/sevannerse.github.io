b9b2482d21f61dbdf79369073fcb88270c63f374
Sevan Nerse <nerse.sevan@gmail.com>
2022-01-13 22:26:17 +0300

    contacts page added

---
+++ pages/_lang/contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <article>
+    <h1>{{ page.title }}</h1>
+    <nuxt-content :document="page" />
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  layout ({ params }) {
+    return params.lang
+  },
+  async asyncData ({ $content, params }) {
+    const page = await $content('/' + params.lang, 'contact').fetch()
+
+    return {
+      page
+    }
+  }
+}
+</script>
