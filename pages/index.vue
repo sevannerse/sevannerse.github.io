12468678e5380ebd14a24c18fad9a46aae62e9d6
Sevan Nerse <nerse.sevan@gmail.com>
2022-01-13 22:26:28 +0300

    middleware added to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,13 @@
 <template>
-  <article>
-    <h1>{{ page.title }}</h1>
-    <nuxt-content :document="page" />
-  </article>
 </template>
 
 <script>
 export default {
   name: 'Index',
-  layout: 'en',
-  async asyncData ({ $content, params }) {
-    const page = await $content('/en', 'index').fetch();
-
-    return {
-      page
-    }
-  }
+  middleware: [
+    function ({ redirect }) {
+      redirect('/en')
+    },
+  ]
 }
 </script>
