82f764d6d7f5abfded00b4ade992616735ed08d9
sevannerse <sevannerse@users.noreply.github.com>
2022-01-13 20:37:37 +0000

    deploy: aeb567202207287a904c70cf0c296be864da5f03

--- _nuxt/e7f5cf5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{270:function(e,n,t){"use strict";t.r(n);var r={name:"Index",middleware:[function(e){(0,e.redirect)("/en")}]},c=t(21),component=Object(c.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
