2aa01802ed2b4848be7eafb494c25d1a842fe593
sevannerse <sevannerse@users.noreply.github.com>
2022-01-14 09:32:37 +0000

    deploy: 62cdeaba8ade5d96fdc75579cb2e3d3867cd53b4

--- _nuxt/static/1642106256/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1642106256",layout:"en",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ab3d9229"}}}}(null,"\u002F"));
